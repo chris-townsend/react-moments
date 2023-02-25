0fb412bcc0e2654bd9e1f5f2767317ed5fdecab8
chris-townsend <chris-townsend@live.co.uk>
2023-02-25 11:52:37 +0000

    Add infinite scroll to comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,27 +1,80 @@
-import React from 'react'
-import styles from '../../styles/Comment.module.css';
-import { Media } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { Avatar } from '../../components/Avatar';
+import React, { useState } from "react";
+import { Media } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Avatar } from "../../components/Avatar";
+import { MoreDropdown } from "../../components/MoreDropdown";
+import styles from "../../styles/Comment.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { axiosRes } from "../../api/axiosDefaults";
+import CommentEditForm from "./CommentEditForm";
 
 const Comment = (props) => {
-    const { profile_id, profile_image, owner, updated_at, content } = props;
-  
-    return (
-      <div>
-        <hr />
-        <Media>
-          <Link to={`/profiles/${profile_id}`}>
-            <Avatar src={profile_image} />
-          </Link>
-          <Media.Body className="align-self-center ml-2">
-            <span className={styles.Owner}>{owner}</span>
-            <span className={styles.Date}>{updated_at}</span>
-            <p>{content}</p>
-          </Media.Body>
-        </Media>
-      </div>
-    );
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_at,
+    content,
+    id,
+    setPost,
+    setComments,
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  const [showEditForm, setShowEditForm] = useState(false);
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {}
   };
-  
-  export default Comment;
+
+  return (
+    <>
+      <hr />
+      <Media>
+        <Link to={`/profiles/${profile_id}`}>
+          <Avatar src={profile_image} />
+        </Link>
+        <Media.Body className="align-self-center ml-2">
+          <span className={styles.Owner}>{owner}</span>
+          <span className={styles.Date}>{updated_at}</span>
+          {showEditForm ? (
+            <CommentEditForm
+              id={id}
+              profile_id={profile_id}
+              content={content}
+              profileImage={profile_image}
+              setComments={setComments}
+              setShowEditForm={setShowEditForm}
+            />
+          ) : (
+            <p>{content}</p>
+          )}
+        </Media.Body>
+        {is_owner && !showEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
+        )}
+      </Media>
+    </>
+  );
+};
+
+export default Comment;
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -12,6 +12,9 @@ import Post from "./Post";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Comment from "../comments/Comment";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from '../../components/Asset'
+import {fetchMoreData} from "../../utils/utils";
 
 function PostPage() {
   const { id } = useParams();
@@ -55,9 +58,23 @@ function PostPage() {
             "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map((comment) => (
-              <Comment key={comment.id} {...comment} />
-            ))
+            <InfiniteScroll
+            children={comments.results.map((comment) => (
+              <Comment
+                key={comment.id}
+                {...comment}
+                setPost={setPost}
+                setComments={setComments}
+                />
+            ))}
+
+
+                dataLength={comments.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+              />
+          
           ) : currentUser ? (
             <span>No comments yet, be the first to comment</span>
           ) : (
